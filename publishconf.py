7edb32af19f0b3f0a00b102e6045917204ef3bf1
TakesxiSximada <takesxi.sximada@gmail.com>
2016-01-09 22:09:16 +0900

    urlを修正

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://takesxisximada.github.io/sximada.2016'
+SITEURL = 'https://takesxisximada.github.io/sximada.2016/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
